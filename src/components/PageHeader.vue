c33c57355e2dc2d048ee5ea41cd4aabbcf30024a
Arild Matsson <arild.matsson@gu.se>
2021-03-08 19:13:42 +0100

    Missing files

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="page-header">
+    <h1><router-link to="/">Somali speech corpus</router-link></h1>
+    <router-link to="/about">About</router-link>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "PageHeader",
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  display: flex;
+  align-items: baseline;
+}
+
+h1 {
+  flex: 1;
+  margin-top: 0;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <article>
+    <p>
+      Ut do occaecat dolore sit Lorem laboris deserunt aliqua id veniam non.
+      Minim quis est dolore amet labore et ea adipisicing aute. Cillum labore
+      est eu ut non labore. Dolore ipsum culpa culpa eu et ullamco nostrud
+      incididunt commodo. Labore fugiat labore enim elit elit enim occaecat.
+      Commodo consectetur cupidatat eu consequat eu.
+    </p>
+    <h2>Consectetur eiusmod enim incididunt</h2>
+    <p>
+      Aliqua labore sunt nostrud sunt cillum est amet eiusmod anim tempor
+      nostrud excepteur sit consectetur. Elit aliqua ea incididunt irure est
+      culpa elit commodo. Eu qui ad dolore sunt non commodo consectetur
+      incididunt.
+    </p>
+  </article>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
