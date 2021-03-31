87e0159ff3246f38a9a47bb6ea4f34a189a7471b
Arild Matsson <arild.matsson@gu.se>
2021-03-31 09:52:55 +0200

    No regex lookahead, Safari doesnt support

--- src/components/Morpheme.vue
+++ src/components/Morpheme.vue
@@ -20,9 +20,9 @@ export default {
           // Superscript numbers.
           .replace(/ (\d+)$/, "<sup>$1</sup>")
           // Italicize capitals unless preceding lowercase.
-          .replace(/([A-Z]+)(?![a-z])/g, "<em>$1</em>")
+          .replace(/([A-Z][A-Z()]*)([^a-z]|$)/g, "<em>$1</em>$2")
           // Italicize capitals following a dash.
-          .replace(/(?<=-)([A-Z]+)/g, "<em>$1</em>")
+          .replace(/(-)([A-Z]+)/g, "$1<em>$2</em>")
       );
     },
   },
